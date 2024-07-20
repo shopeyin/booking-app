94a9f36026e78f83b057ebf210441a3c4195c818
shopeyin <49595007+shopeyin@users.noreply.github.com>
2024-07-20 19:53:25 +0100

    added cabins list functionality

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,14 @@
 // import Counter from "@/app/_components/Counter";
-// import CabinCard from "@/app/_components/CabinCard";
+
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
   title: "Cabins",
 };
 
 export default function Page() {
-  // CHANGE
-  const cabins = [];
-
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,14 +22,9 @@ export default function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {/* {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))} */}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
